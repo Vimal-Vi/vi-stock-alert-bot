115f1a896cf7dcb18d30b0cb581bd29e51e1cb22
Vimal-Vi <140502040+Vimal-Vi@users.noreply.github.com>
2026-09-01 16:12:58 +0530

    Implement Telegram alert on bot start
    Added functionality to send a Telegram message when the bot starts.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-requests
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+message = "🚀 NSE 14% Alert Bot Started!"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Alert Sent")
