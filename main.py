35182f4fc87330333ca2f73087971eb30b287e71
Vimal-Vi <140502040+Vimal-Vi@users.noreply.github.com>
2026-09-01 16:09:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import os
-import requests
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-message = "🚀 Stock Alert Bot Working Successfully!"
-
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": message
-})
-
-print("Message Sent")
+requests
